e4f1d094795a071d571ba18db0cec2ff5ebda8f0
kamuramaki@gmail.com <kamuramaki@gmail.com>
2023-02-13 01:01:22 +0900

    空のcommit

---
+++ index.js
@@ -0,0 +1,16 @@
+// コアバージョンに加えて、ナビゲーションとページネーションを追加する
+console.log("dd");
+const swiper = new Swiper(".swiper", {
+	direction: 'vertical',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
